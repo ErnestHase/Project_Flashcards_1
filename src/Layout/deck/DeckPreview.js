6e995244e12e833f0e27a996febe33723d3d1f77
Ernest Hase <ernhase@gmail.com>
2021-10-20 22:56:32 -0500

    error and forms coded

--- src/Layout/deck/DeckPreview.js
+++ src/Layout/deck/DeckPreview.js
@@ -1,2 +1,2 @@
 import React from "react";
-import Proptypes from "prop-types"
+import Proptypes from "prop-types";
--- src/Layout/error/NotEnoughCards.js
+++ src/Layout/error/NotEnoughCards.js
@@ -1,2 +1,21 @@
 import React from "react";
 import Proptypes from "prop-types"
+import Buttons from "../comp/Buttons"
+
+function NotEnoughCards({length, id}) {
+    return (
+        <div id="card-error">
+            <h3>Not Enough Cards</h3>
+            <p>You need atleast 3 cards to study. There are {length} cards in this deck.</p>
+
+            <Buttons names={["add-card"]} deckId={id} />
+        </div>
+    )
+}
+
+NotEnoughCards.propTypes = {
+    length: Proptypes.number.isRequired,
+    id: Proptypes.number.isRequired,
+}
+
+export default NotEnoughCards
